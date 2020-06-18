ac84e460cbc02719e4ac76153dc5a4dd5b0aebe2
Ara Jo <arajodeveloper@gmail.com>
2020-06-18 15:49:28 -0700

    img not showing

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,25 +4,25 @@ cats = [
     name: 'Hongcha',
     age: 2,
     enjoys: 'Exploring backyards ^-^',
-    image_path: 'src/images/hongcha.jpg' 
+    image_path: 'https://i.imgur.com/RJExRjB.jpg' 
   },
   {
     name: 'Captain Whiskers',
     age: 5,
     enjoys: 'Licking myself when no one is looking',
-    image_path: 'src/images/hongcha2.PNG' 
+    image_path: 'https://i.imgur.com/qBRjUrX.jpg' 
   },
   {
     name: 'Gangstah',
     age: 8,
     enjoys: 'Stealing tuna cans from other cats',
-    image_path: 'src/images/fat-cat.jpg' 
+    image_path: 'https://i.imgur.com/1nuiJYb.jpg' 
   },
   {
     name: 'Mr.Tuxedo',
     age: 3,
     enjoys: 'Settling down with another cat',
-    image_path: 'src/images/hongcha.jpg' 
+    image_path: 'https://i.imgur.com/hm7f0OY.jpg' 
   }
 
 ]
